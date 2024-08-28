403daccbfde836816b91e1201310d325bd1c6e59
Rebeca Aguiar - pcifba <beca.cout@gmail.com>
2024-08-28 17:09:38 -0300

    todo: create User Model

---
+++ src/main/java/io/github/becxagy/s3_file/domain/User.java
@@ -0,0 +1,46 @@
+package io.github.becxagy.s3_file.domain;
+
+public class User {
+
+    private Long id;
+    private String imageUrl;
+    private String name;
+    private String email;
+
+   public User(){
+
+   }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
