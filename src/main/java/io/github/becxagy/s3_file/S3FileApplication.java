ec32665a9777ed0612886356ef140eac79c42006
Rebeca Aguiar - pcifba <beca.cout@gmail.com>
2024-08-28 17:06:19 -0300

    todo: Aws S3 basic configuration

---
+++ src/main/java/io/github/becxagy/s3_file/S3FileApplication.java
@@ -0,0 +1,13 @@
+package io.github.becxagy.s3_file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3FileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3FileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/becxagy/s3_file/S3FileApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.becxagy.s3_file;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3FileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
