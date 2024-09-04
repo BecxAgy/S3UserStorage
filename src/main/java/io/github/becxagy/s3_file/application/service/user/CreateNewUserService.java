3735d0377af271c18f078090dbd6157b944670cd
Rebeca Aguiar - pcifba <beca.cout@gmail.com>
2024-09-04 15:53:30 -0300

    todo: ports and adapter implementations for create new user with image url

--- src/main/java/io/github/becxagy/s3_file/application/service/user/CreateNewUserService.java
+++ src/main/java/io/github/becxagy/s3_file/application/service/user/CreateNewUserService.java
@@ -2,7 +2,9 @@ package io.github.becxagy.s3_file.application.service.user;
 
 import io.github.becxagy.s3_file.application.usecase.user.CreateUserUseCase;
 import io.github.becxagy.s3_file.domain.User;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CreateNewUserService implements CreateUserUseCase {
     @Override
     public void create(User user) {
